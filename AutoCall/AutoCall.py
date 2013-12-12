e8dc280829a19aabacd16a205210a8660413242c
M.R.Z <zgd1348833@gmail.com>
2013-12-13 00:42:17 +0800

    Add Camera Testing script.

---
+++ AutoCall/AutoCall.py
@@ -0,0 +1,104 @@
+#########################################################################
+# This is the test for Auto call, just for speaker's keepon test.
+# Develop by zgd1348833@gmail.com
+# This is just for T8702A (Hisense)
+#########################################################################
+# import the monkeyrunner modules used by this program
+import time;
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+from com.android.monkeyrunner.easy import EasyMonkeyDevice, By
+
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+device.press('KEYCODE_HOME','DOWN_AND_UP')
+
+# t-Shark postition
+# call icon position in lanucher
+PHONE_ICON_X = 64;
+PHONE_ICON_Y = 789;
+
+# call icon position in callboard
+CALL_ICON_X = 117;
+CALL_ICON_Y = 722;
+
+# sim card selection
+SIM1_ICON_X = 226;
+SIM1_ICON_Y = 449;
+SIM2_ICON_X = 226;
+SIM2_ICON_Y = 537;
+
+# end call
+END_ICON_X = 247;
+END_ICON_Y = 792;
+
+testCount = 10000;
+
+
+for i in range(0, testCount):
+	MonkeyRunner.sleep(2);
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+
+	# open phone from lanucher
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Openning phone call dial..." %(t,i)
+	device.touch(PHONE_ICON_X, PHONE_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(1);
+
+	# click on call icon
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]click dial pad..." %(t,i)
+	device.touch(CALL_ICON_X, CALL_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(1);
+	device.touch(CALL_ICON_X, CALL_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(2);
+
+	# select sim1
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]select a sim1 card..." %(t,i)
+	device.touch(SIM1_ICON_X, SIM1_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(15);
+
+	# end call
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]end call..." %(t,i)
+	device.touch(END_ICON_X, END_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+	MonkeyRunner.sleep(1);
+	device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+	MonkeyRunner.sleep(1);
+
+	#================================================
+	# open phone from lanucher
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Openning phone call dial..." %(t,i)
+	device.touch(PHONE_ICON_X, PHONE_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(1);
+
+	# click on call icon
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]click dial pad..." %(t,i)
+	device.touch(CALL_ICON_X, CALL_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(1);
+	device.touch(CALL_ICON_X, CALL_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(2);
+
+	# select sim1
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]select a sim2 card..." %(t,i)
+	device.touch(SIM2_ICON_X, SIM2_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	MonkeyRunner.sleep(15);
+
+	# end call
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]end call..." %(t,i)
+	device.touch(END_ICON_X, END_ICON_Y, MonkeyDevice.DOWN_AND_UP);
+	device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+	MonkeyRunner.sleep(1);
+	device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+	MonkeyRunner.sleep(1);
+
+	# this is finished
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]This test is finished." %(t,i)
+print "Test finished."
---
+++ AutoCamera/Camera.py
@@ -0,0 +1,48 @@
+# import the monkeyrunner modules used by this program
+import time;
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+from com.android.monkeyrunner.easy import EasyMonkeyDevice, By
+
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+device.press('KEYCODE_HOME','DOWN_AND_UP')
+
+# t-Shark postition
+POST_X = 240;
+POST_Y = 744;
+
+testCount = 10;
+
+for i in range(0, testCount):
+	MonkeyRunner.sleep(2);
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+	# open Camera
+	# android 4.0 8825cd
+	#device.startActivity(component='com.android.camera/.Camera');
+	# android 4.1 tshark
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
+	device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
+
+	MonkeyRunner.sleep(1);
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
+
+	device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
+	#easy_device = EasyMonkeyDevice(device);
+	#easy_device.touch(By.id('id/shutter_button'), MonkeyDevice.DOWN_AND_UP);
+
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
+
+	MonkeyRunner.sleep(4);
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
+
+	# exit Camera
+	device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] [%04d]This test is finished." %(t,i)
+print "Test finished."
