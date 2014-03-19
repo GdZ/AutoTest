037cb99da03cd713d3260adaccb26fba479c94db
M.R.Z <zgd1348833@gmail.com>
2014-03-19 18:15:43 +0800

    Add for huawei camera test

--- AutoCamera/Camera.py
+++ AutoCamera/Camera.py
@@ -48,59 +48,103 @@ from com.android.monkeyrunner.easy import EasyMonkeyDevice, By
 device = MonkeyRunner.waitForConnection()
 device.press('KEYCODE_HOME','DOWN_AND_UP')
 
-# t-Shark postition
-POST_X = 240;
-POST_Y = 744;
-
-testCount = 500;
-
-for i in range(0, testCount):
-	MonkeyRunner.sleep(2);
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
-	# open Camera
-	# android 4.0 8825cd
-	#device.startActivity(component='com.android.camera/.Camera');
-	# android 4.1 tshark
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
-	device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
-
-	MonkeyRunner.sleep(2);
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
-
-	#/////////////////////////////////////////////////////////////////////////
-	#
-	#    EasyMonkeyDevice cannot be used in some version of phone, so replaced
-	# by touch point position.
-	#
-	#------------------------------------------------------------------------
-	#easy_device = EasyMonkeyDevice(device);
-	#easy_device.touch(By.id('id/shutter_button'), MonkeyDevice.DOWN_AND_UP);
-	#------------------------------------------------------------------------
-	device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
-	#/////////////////////////////////////////////////////////////////////////
-
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
-
-	MonkeyRunner.sleep(4);
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
-
-	# exit Camera
-	ret = device.press('KEYCODE_BACK', 'DOWN_AND_UP');
-	print "%s [DEBUG] [%04d]ret:%s" %(t,i,ret)
-
-	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-	print "%s [DEBUG] [%04d]This test is finished." %(t,i)
-	'''
-	if i>5:
-		device = device;
+def sprdCamera():
+	# t-Shark postition
+	POST_X = 240;
+	POST_Y = 744;
+	testCount = 1000;
+	for i in range(0, testCount):
+		MonkeyRunner.sleep(2);
 		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [INFO] [%04d]Reconnect to device." %(t,i)
-	else:
+		print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+		# open Camera
+		# android 4.0 8825cd
+		#device.startActivity(component='com.android.camera/.Camera');
+		# android 4.1 tshark
 		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [INFO] [%04d]Not connect to device." %(t,i)
-	'''
+		print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
+		## this is for sprd test.
+		device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
+
+		MonkeyRunner.sleep(2);
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
+
+		#/////////////////////////////////////////////////////////////////////////
+		#
+		#    EasyMonkeyDevice cannot be used in some version of phone, so replaced
+		# by touch point position.
+		#
+		#------------------------------------------------------------------------
+		#easy_device = EasyMonkeyDevice(device);
+		#easy_device.touch(By.id('id/shutter_button'), MonkeyDevice.DOWN_AND_UP);
+		#------------------------------------------------------------------------
+		device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
+		#/////////////////////////////////////////////////////////////////////////
+
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
+
+		MonkeyRunner.sleep(4);
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
+
+		# exit Camera
+		ret = device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+		print "%s [DEBUG] [%04d]ret:%s" %(t,i,ret)
+
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]This test is finished." %(t,i)
+
+def huaweiCamera():
+	# t-Shark postition
+	POST_X = 240;
+	POST_Y = 744;
+	testCount = 1000;
+	for i in range(0, testCount):
+		MonkeyRunner.sleep(2);
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+		# open Camera
+		# android 4.0 8825cd
+		#device.startActivity(component='com.android.camera/.Camera');
+		# android 4.1 tshark
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
+		## this is for sprd test.
+		#device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
+		## this is for huawei test
+		device.startActivity(component='com.android.gallery3d/com.android.camera.CameraLauncher')
+
+		MonkeyRunner.sleep(2);
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
+
+		#/////////////////////////////////////////////////////////////////////////
+		#
+		#    EasyMonkeyDevice cannot be used in some version of phone, so replaced
+		# by touch point position.
+		#
+		#------------------------------------------------------------------------
+		#easy_device = EasyMonkeyDevice(device);
+		#easy_device.touch(By.id('id/shutter_button'), MonkeyDevice.DOWN_AND_UP);
+		#------------------------------------------------------------------------
+		device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
+		#/////////////////////////////////////////////////////////////////////////
+
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
+
+		MonkeyRunner.sleep(4);
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
+
+		# exit Camera
+		ret = device.press('KEYCODE_BACK', 'DOWN_AND_UP');
+		print "%s [DEBUG] [%04d]ret:%s" %(t,i,ret)
+
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		print "%s [DEBUG] [%04d]This test is finished." %(t,i)
+
+#sprdCamera();
+huaweiCamera();
