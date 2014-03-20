6c1bcaf9fff88f7f2a7280eed9e3dbb0c30c120d
M.R.Z <zgd1348833@gmail.com>
2014-03-20 15:24:18 +0800

    Modify for new log system

--- AutoCamera/Camera.py
+++ AutoCamera/Camera.py
@@ -44,31 +44,40 @@ import time;
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 from com.android.monkeyrunner.easy import EasyMonkeyDevice, By
 
-# Connects to the current device, returning a MonkeyDevice object
-device = MonkeyRunner.waitForConnection()
-device.press('KEYCODE_HOME','DOWN_AND_UP')
+def LOGD(TAG, msg):
+	t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+	print "%s [DEBUG] %s: %s" %(t, TAG, msg)
+
+def doClick(keycode, action):
+	TAG = "doClick"
+	LOGD(TAG, "Do " + keycode + " click " + action)
+	device.press(keycode, action);
+	MonkeyRunner.sleep(1);
 
 def sprdCamera():
 	# t-Shark postition
+	TAG = "sprdCamera"
 	POST_X = 240;
 	POST_Y = 744;
 	testCount = 1000;
 	for i in range(0, testCount):
+		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
+		LOGD(TAG, str(i) + ':Begin a new take photo test...')
+		doClick('KEYCODE_BACK', 'DOWN_AND_UP');
+		MonkeyRunner.sleep(1);
+		doClick('KEYCODE_BACK', 'DOWN_AND_UP');
+
 		MonkeyRunner.sleep(2);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+		LOGD(TAG, str(i) + ':This test is beginning...')
 		# open Camera
-		# android 4.0 8825cd
+		# sprd android 4.0 8825c
 		#device.startActivity(component='com.android.camera/.Camera');
-		# android 4.1 tshark
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
-		## this is for sprd test.
+		# sprd android 4.1 tshark
+		LOGD(TAG, str(i) + ":Openning Camera...");
 		device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
 
 		MonkeyRunner.sleep(2);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
+		LOGD(TAG, str(i) + ":Waiting for take photo...")
 
 		#/////////////////////////////////////////////////////////////////////////
 		#
@@ -81,44 +90,36 @@ def sprdCamera():
 		#------------------------------------------------------------------------
 		device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
 		#/////////////////////////////////////////////////////////////////////////
-
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
+		LOGD(TAG, str(i) + ":Have take photo...")
 
 		MonkeyRunner.sleep(4);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
+		LOGD(TAG, str(i) + ":Exiting Camera...")
 
 		# exit Camera
-		ret = device.press('KEYCODE_BACK', 'DOWN_AND_UP');
-		print "%s [DEBUG] [%04d]ret:%s" %(t,i,ret)
-
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]This test is finished." %(t,i)
+		doClick('KEYCODE_BACK', 'DOWN_AND_UP');
+		LOGD(TAG, str(i) + ":This test is finished.")
 
 def huaweiCamera():
 	# t-Shark postition
+	TAG = "huaweiCamera"
 	POST_X = 240;
 	POST_Y = 744;
 	testCount = 1000;
 	for i in range(0, testCount):
 		MonkeyRunner.sleep(2);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]This test is beginning..." %(t,i)
+		LOGD(TAG, str(i) + ":This test is beginning...")
 		# open Camera
 		# android 4.0 8825cd
 		#device.startActivity(component='com.android.camera/.Camera');
 		# android 4.1 tshark
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Openning Camera..." %(t,i)
+		LOGD(TAG, str(i) + ":Openning Camera...")
 		## this is for sprd test.
 		#device.startActivity(component='com.android.gallery3d/com.android.camera.Camera')
 		## this is for huawei test
 		device.startActivity(component='com.android.gallery3d/com.android.camera.CameraLauncher')
 
 		MonkeyRunner.sleep(2);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Waiting for take photo..." %(t,i)
+		LOGD(TAG, str(i) + ":Waiting for take photo...")
 
 		#/////////////////////////////////////////////////////////////////////////
 		#
@@ -131,20 +132,19 @@ def huaweiCamera():
 		#------------------------------------------------------------------------
 		device.touch(POST_X, POST_Y, MonkeyDevice.DOWN_AND_UP);
 		#/////////////////////////////////////////////////////////////////////////
-
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Have take photo..." %(t,i)
+		LOGD(TAG, str(i) + ":Have take photo...")
 
 		MonkeyRunner.sleep(4);
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]Exiting Camera..." %(t,i)
+		LOGD(TAG, str(i) + ":Exiting Camera...")
 
 		# exit Camera
-		ret = device.press('KEYCODE_BACK', 'DOWN_AND_UP');
-		print "%s [DEBUG] [%04d]ret:%s" %(t,i,ret)
+		doClick('KEYCODE_BACK', 'DOWN_AND_UP');
+		LOGD(TAG, str(i) + ":This test is finished.")
 
-		t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()));
-		print "%s [DEBUG] [%04d]This test is finished." %(t,i)
 
-#sprdCamera();
-huaweiCamera();
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+doClick('KEYCODE_HOME','DOWN_AND_UP')
+#LOGD('test', 'this is the test....')
+sprdCamera();
+#huaweiCamera();
